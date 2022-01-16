fc6cbbafb50dc05b7db8d97162bc8d7b7bad68dc
oansky <oana.anghelachi@gmail.com>
2022-01-16 20:57:52 +0100

    Set up the API

---
+++ app.js
@@ -0,0 +1,7 @@
+// https://www.edamam.com/
+
+// Get all the elements we need
+const appID = "a644b2bd"
+const appKey = "91082fb60145cdf52bfa79fe99590457"
+const recipeURL = 'https://api.edamam.com/api/recipes/v2'
+
